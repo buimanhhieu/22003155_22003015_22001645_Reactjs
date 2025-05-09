df109eb547a8dda407ea723ae6987af26faf67e1
lephanquocdai <qdai1209@gmail.com>
2025-05-10 02:46:58 +0700

    Update

--- src/pages/User/user.slice.jsx
+++
@@ -1,8 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit"
-import { payloadCreator } from "src/utils/helper"
-import purchaseApi from "src/api/purchase.api"
-
-export const getPurchase = createAsyncThunk(
-  "user/getPurchases",
-  payloadCreator(purchaseApi.getPurchases)
-)
